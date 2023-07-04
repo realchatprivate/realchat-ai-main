5d05914d29b40fd7336578edd95ac2f78959d884
Rémy Menard <remymenard.work@gmail.com>
2023-07-04 21:49:23 +0200

    fix upload audio api call

--- src/pages/api/dialerai.jsx
+++ src/pages/api/dialerai.jsx
@@ -2,14 +2,7 @@ import * as https from 'https'
 import * as fs from 'fs'
 import * as path from 'path'
 import FormData from 'form-data'
-
-const username = 'leporecolby5@gmail.com' // replace with your actual username
-const password = 'bFc3jG9Py' // replace with your actual password
-const hostname_ip = 'autodialer.dialer.ai' // replace with your actual hostname or IP
-
-const url =
-  'https://realchat-mp3s.s3.us-east-2.amazonaws.com/audio-1688395715406.mp3'
-const filePath = path.resolve('./', path.basename(url))
+import axios from 'axios'
 
 const downloadFile = (url, dest) =>
   new Promise((resolve, reject) => {
@@ -28,46 +21,49 @@ const downloadFile = (url, dest) =>
       })
   })
 
-const uploadFile = async filePath => {
+const uploadFile = async (filePath, fileName) => {
   // Read the file from filesystem
   const fileStream = fs.createReadStream(filePath)
 
   // Create a form
   const form = new FormData()
-  form.append('name', 'audio created from API')
+  form.append('name', fileName)
   form.append('audio_file', fileStream)
-  form.append('user_id', '1701')
+  form.append('user_id', '1')
 
   // Calculate Base64 for Authorization
   const base64Credentials = Buffer.from(
     `${process.env.DIALERAI_BASIC_AUTH_LOGIN}:${process.env.DIALERAI_BASIC_AUTH_PASSWORD}`
   ).toString('base64')
 
-  // Prepare the form data for the fetch body and get the content-type header
-  const formHeaders = form.getHeaders()
-
   // Make the API request
-  const response = await fetch(
-    'https://autodialer.dialer.ai/rest-api/audio-files/',
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Basic ${base64Credentials}`,
-        ...formHeaders
-      },
-      body: form
-    }
-  )
+  const response = await axios({
+    method: 'post',
+    url: 'https://dialer.realchat.ai/rest-api/audio-files/',
+    data: form,
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Basic ${base64Credentials}`,
+      ...form.getHeaders()
+    },
+    maxRedirects: 0 // This will disable following redirects
+  })
+
+  return response.data
 
   // Read the API response
-  return await response.json()
 }
 
 export default async function handler (req, res) {
   try {
+    const { phone, firstName, lastName, url } = req.body
+
+    const fileName = `${firstName} ${lastName} - ${phone}`
+
+    const filePath = path.resolve('./', path.basename(url))
+
     await downloadFile(url, filePath)
-    const result = await uploadFile(filePath)
+    const result = await uploadFile(filePath, fileName)
     res.status(200).json(result)
   } catch (error) {
     res.status(500).json({ error: error.message })
